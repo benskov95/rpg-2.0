9077670e9e362df743851db57a4c619048279839
Benjamin S <ben.sommer.skovgaard@gmail.com>
2022-06-02 17:40:21 +0200

    added armor inventory to equipment page

--- src/components/Equipment.js
+++ src/components/Equipment.js
@@ -1,4 +1,5 @@
 import "./css/Equipment.css"
+import iconConverter from "./utility/iconConverter";
 
 export default function Equipment() {
 
@@ -21,35 +22,35 @@ export default function Equipment() {
             <div id="equipment-box">
                 <ul id="equipment-list-r">
                     <li className="equipment-list-element" onMouseOver={showTooltip} onMouseLeave={hideTooltip}>
-                        <img className="equipment-list-element-img" src="https://cdn4.iconfinder.com/data/icons/video-game-items-concepts/128/armor-gloves-512.png" alt="" />
+                        <img className="equipment-list-element-img" src={iconConverter["gloves"]} alt="" />
                         <div className="tooltip" hidden={true}>
                             <p className="tooltip-name">Name</p>
                             <p className="tooltip-description">+10 stam<br />+10 int</p>
                         </div>
                     </li>
                     <li className="equipment-list-element" onMouseOver={showTooltip} onMouseLeave={hideTooltip}>
-                        <img className="equipment-list-element-img" src="https://icon-library.com/images/pants-icon-png/pants-icon-png-16.jpg" alt="" />
+                        <img className="equipment-list-element-img" src={iconConverter["legs"]} alt="" />
                         <div className="tooltip" hidden={true}>
                             <p className="tooltip-name">Name</p>
                             <p className="tooltip-description">+10 stam<br />+10 int</p>
                         </div>
                     </li>
                     <li className="equipment-list-element" onMouseOver={showTooltip} onMouseLeave={hideTooltip}>
-                        <img className="equipment-list-element-img" src="https://cdn2.iconfinder.com/data/icons/rpg-fantasy-game-basic-ui/512/equipment_costume_armor_boot_foot_metal_knight_warrior_protection_leg_2-512.png" alt="" />
+                        <img className="equipment-list-element-img" src={iconConverter["boots"]} alt="" />
                         <div className="tooltip" hidden={true}>
                             <p className="tooltip-name">Name</p>
                             <p className="tooltip-description">+10 stam<br />+10 int</p>
                         </div>
                     </li>
                     <li className="equipment-list-element" onMouseOver={showTooltip} onMouseLeave={hideTooltip}>
-                        <img className="equipment-list-element-img" src="https://cdn2.iconfinder.com/data/icons/rpg-fantasy-game-basic-ui/512/equipment_accessory_ring_wedding_gold_jewelry_gift_love-512.png" alt="" />
+                        <img className="equipment-list-element-img" src={iconConverter["ring"]} alt="" />
                         <div className="tooltip" hidden={true}>
                             <p className="tooltip-name">Name</p>
                             <p className="tooltip-description">+10 stam<br />+10 int</p>
                         </div>
                     </li>
                     <li className="equipment-list-element" onMouseOver={showTooltip} onMouseLeave={hideTooltip}>
-                        <img className="equipment-list-element-img" src="https://cdn2.iconfinder.com/data/icons/rpg-fantasy-game-basic-ui/512/equipment_accessory_ring_wedding_gold_jewelry_gift_love-512.png" alt="" />
+                        <img className="equipment-list-element-img" src={iconConverter["ring"]} alt="" />
                         <div className="tooltip" hidden={true}>
                             <p className="tooltip-name">Name</p>
                             <p className="tooltip-description">+10 stam<br />+10 int</p>
@@ -59,35 +60,35 @@ export default function Equipment() {
 
                 <ul id="equipment-list-l">
                     <li className="equipment-list-element" onMouseOver={showTooltip} onMouseLeave={hideTooltip}>
-                        <img className="equipment-list-element-img" src="https://cdn2.iconfinder.com/data/icons/rpg-fantasy-game-basic-ui/512/head_helmet_armor_warrior_knight_helm-512.png" alt="" />
+                        <img className="equipment-list-element-img" src={iconConverter["helm"]} alt="" />
                         <div className="tooltip" hidden={true}>
                             <p className="tooltip-name">Name</p>
                             <p className="tooltip-description">+10 stam<br />+10 int</p>
                         </div>
                     </li>
                     <li className="equipment-list-element" onMouseOver={showTooltip} onMouseLeave={hideTooltip}>
-                        <img className="equipment-list-element-img" src="https://cdn1.iconfinder.com/data/icons/rpg-game-design/100/rpg_necklace-512.png" alt="" />
+                        <img className="equipment-list-element-img" src={iconConverter["necklace"]} alt="" />
                         <div className="tooltip" hidden={true}>
                             <p className="tooltip-name">Name</p>
                             <p className="tooltip-description">+10 stam<br />+10 int</p>
                         </div>
                     </li>
                     <li className="equipment-list-element" onMouseOver={showTooltip} onMouseLeave={hideTooltip}>
-                        <img className="equipment-list-element-img" src="https://cdn-icons-png.flaticon.com/512/5101/5101797.png" alt="" />
+                        <img className="equipment-list-element-img" src={iconConverter["cloak"]} alt="" />
                         <div className="tooltip" hidden={true}>
                             <p className="tooltip-name">Name</p>
                             <p className="tooltip-description">+10 stam<br />+10 int</p>
                         </div>
                     </li>
                     <li className="equipment-list-element" onMouseOver={showTooltip} onMouseLeave={hideTooltip}>
-                        <img className="equipment-list-element-img" src="https://cdn-icons-png.flaticon.com/512/4820/4820460.png" alt="" />
+                        <img className="equipment-list-element-img" src={iconConverter["chest"]} alt="" />
                         <div className="tooltip" hidden={true}>
                             <p className="tooltip-name">Name</p>
                             <p className="tooltip-description">+10 stam<br />+10 int</p>
                         </div>
                     </li>
                     <li className="equipment-list-element" onMouseOver={showTooltip} onMouseLeave={hideTooltip}>
-                        <img className="equipment-list-element-img" src="https://cdn4.iconfinder.com/data/icons/video-game-items-concepts/128/armor-bracers-2-512.png" alt="" />
+                        <img className="equipment-list-element-img" src={iconConverter["bracers"]} alt="" />
                         <div className="tooltip" hidden={true}>
                             <p className="tooltip-name">Name</p>
                             <p className="tooltip-description">+10 stam<br />+10 int</p>
@@ -96,12 +97,58 @@ export default function Equipment() {
                 </ul>
 
                 <div id="weapon" onMouseOver={showTooltip} onMouseLeave={hideTooltip}>
-                    <img id="weapon-img" src="https://icon-library.com/images/weapon-icon/weapon-icon-12.jpg" alt="" />
+                    <img id="weapon-img" src={iconConverter["weapon"]} alt="" />
                     <div id="weapon-tooltip" hidden={true}>
                         <p className="tooltip-name">Name</p>
                         <p className="tooltip-description">+10 stam<br />+10 int</p>
                     </div>
                 </div>
+            </div>
+
+            <div id="armor-inventory">
+                <div id="armor-inventory-filter">
+                    <img className="armor-inventory-filter-option" style={{border: "1px solid white"}} src={iconConverter["bag"]} alt="" />
+                    <img className="armor-inventory-filter-option" src={iconConverter["weapon"]} alt="" />
+                    <img className="armor-inventory-filter-option" src={iconConverter["helm"]} alt="" />
+                    <img className="armor-inventory-filter-option" src={iconConverter["chest"]} alt="" />
+                    <img className="armor-inventory-filter-option" src={iconConverter["cloak"]} alt="" />
+                    <img className="armor-inventory-filter-option" src={iconConverter["bracers"]} alt="" />
+                    <img className="armor-inventory-filter-option" src={iconConverter["gloves"]} alt="" />
+                    <img className="armor-inventory-filter-option" src={iconConverter["legs"]} alt="" />
+                    <img className="armor-inventory-filter-option" src={iconConverter["boots"]} alt="" />
+                    <img className="armor-inventory-filter-option" src={iconConverter["necklace"]} alt="" />
+                    <img className="armor-inventory-filter-option" src={iconConverter["ring"]} alt="" />
+                </div>
+                <ul id="armor-inventory-list">
+                    <li className="armor-inventory-list-element">
+                        <img className="armor-inventory-list-element-img" src="https://cdn4.iconfinder.com/data/icons/video-game-items-concepts/128/armor-bracers-2-512.png" alt="" />
+                        <p className="armor-inventory-list-element-text">Gang weed</p>
+                    </li>
+                    <li className="armor-inventory-list-element">
+                        <img className="armor-inventory-list-element-img" src="https://cdn4.iconfinder.com/data/icons/video-game-items-concepts/128/armor-bracers-2-512.png" alt="" />
+                        <p className="armor-inventory-list-element-text">Gang weed</p>
+                    </li>
+                    <li className="armor-inventory-list-element">
+                        <img className="armor-inventory-list-element-img" src="https://cdn4.iconfinder.com/data/icons/video-game-items-concepts/128/armor-bracers-2-512.png" alt="" />
+                        <p className="armor-inventory-list-element-text">Gang weed</p>
+                    </li>
+                    <li className="armor-inventory-list-element">
+                        <img className="armor-inventory-list-element-img" src="https://cdn4.iconfinder.com/data/icons/video-game-items-concepts/128/armor-bracers-2-512.png" alt="" />
+                        <p className="armor-inventory-list-element-text">Gang weed</p>
+                    </li>
+                    <li className="armor-inventory-list-element">
+                        <img className="armor-inventory-list-element-img" src="https://cdn4.iconfinder.com/data/icons/video-game-items-concepts/128/armor-bracers-2-512.png" alt="" />
+                        <p className="armor-inventory-list-element-text">Gang weed</p>
+                    </li>
+                    <li className="armor-inventory-list-element">
+                        <img className="armor-inventory-list-element-img" src="https://cdn4.iconfinder.com/data/icons/video-game-items-concepts/128/armor-bracers-2-512.png" alt="" />
+                        <p className="armor-inventory-list-element-text">Gang weed</p>
+                    </li>
+                    <li className="armor-inventory-list-element">
+                        <img className="armor-inventory-list-element-img" src="https://cdn4.iconfinder.com/data/icons/video-game-items-concepts/128/armor-bracers-2-512.png" alt="" />
+                        <p className="armor-inventory-list-element-text">Gang weed</p>
+                    </li>
+                </ul>
             </div>
         </>
     )
---
+++ src/components/utility/iconConverter.js
@@ -0,0 +1,27 @@
+import bag from "./icons/bag.png";
+import boots from "./icons/boots.png";
+import bracers from "./icons/bracers.png";
+import chest from "./icons/chest.png";
+import cloak from "./icons/cloak.png";
+import gloves from "./icons/gloves.png";
+import helm from "./icons/helm.png";
+import legs from "./icons/legs.png";
+import necklace from "./icons/necklace.png";
+import ring from "./icons/ring.png";
+import weapon from "./icons/weapon.png"
+
+const iconConverter = {
+    "bag": bag,
+    "boots": boots,
+    "bracers": bracers,
+    "chest": chest,
+    "cloak":  cloak,
+    "gloves": gloves,
+    "helm": helm,
+    "legs": legs,
+    "necklace": necklace,
+    "ring": ring,
+    "weapon": weapon
+}
+
+export default iconConverter;
