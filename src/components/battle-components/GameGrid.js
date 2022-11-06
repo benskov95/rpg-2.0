d93c2888fdd8fd0f94d9887d6ff881a6b8ef069a
Benjamin S <ben.sommer.skovgaard@gmail.com>
2022-11-06 20:37:23 +0100

    minor

--- src/components/battle-components/GameGrid.js
+++ src/components/battle-components/GameGrid.js
@@ -13,38 +13,36 @@ export default function GameGrid(props) {
     }, [playerDmg]);
 
     return (
-        <>
-            <div className="game-grid">
-                {pLogic.battleGrid.map((row, i) => {
-                    return (
-                        row.map((cell, j) => {
-                            let idxDiff = i > 0 ? i * 8 : 0;
-                            return (
-                                <div 
-                                className="cell" 
-                                id={`{\"x\": ${j}, \"y\": ${i}}`}
-                                ref={el => props.cellsRef.current[j + idxDiff] = el}
-                                key={cell}>
-                                    {cell === pLogic.battleGrid[props.playerPosition.y][props.playerPosition.x] &&
-                                        <div className="participant">
-                                            <img className="participant-img" src={iconConverter["wizard"]} alt="" />
-                                            <span className="health-label">235</span>
-                                            <progress className="health-bar" ref={playerHpRef} value={100} max={100} />
-                                        </div>
-                                    }
-                                    {cell === pLogic.battleGrid[props.enemyPosition.y][props.enemyPosition.x] &&
-                                        <div className="participant">
-                                            <img className="participant-img" src={iconConverter["wizard"]} alt="" />
-                                            <span className="health-label">1003</span>
-                                            <progress className="health-bar" ref={enemyHpRef} value={100} max={100} /> 
-                                        </div> 
-                                    }
-                                </div>
-                            )
-                        }))
-                    })
-                }
-            </div>
-        </>
+        <div className="game-grid">
+            {pLogic.battleGrid.map((row, i) => {
+                return (
+                    row.map((cell, j) => {
+                        let idxDiff = i > 0 ? i * 8 : 0;
+                        return (
+                            <div 
+                            className="cell" 
+                            id={`{\"x\": ${j}, \"y\": ${i}}`}
+                            ref={el => props.cellsRef.current[j + idxDiff] = el}
+                            key={cell}>
+                                {cell === pLogic.battleGrid[props.playerPosition.y][props.playerPosition.x] &&
+                                    <div className="participant">
+                                        <img className="participant-img" src={iconConverter["wizard"]} alt="" />
+                                        <span className="health-label">235</span>
+                                        <progress className="health-bar" ref={playerHpRef} value={100} max={100} />
+                                    </div>
+                                }
+                                {cell === pLogic.battleGrid[props.enemyPosition.y][props.enemyPosition.x] &&
+                                    <div className="participant">
+                                        <img className="participant-img" src={iconConverter["wizard"]} alt="" />
+                                        <span className="health-label">1003</span>
+                                        <progress className="health-bar" ref={enemyHpRef} value={100} max={100} /> 
+                                    </div> 
+                                }
+                            </div>
+                        )
+                    }))
+                })
+            }
+        </div>
     )
 }
